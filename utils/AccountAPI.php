f047466518f23a37c0b34c2bb37515531e706c10
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2023-09-28 00:07:22 +0300

    Completed account setting page and API

--- utils/AccountAPI.php
+++ utils/AccountAPI.php
@@ -12,47 +12,73 @@
 	//session id needs to be turned into double quoted string to work with MySQL
 	$x = '"' . session_id() . '"';
 	$y = time();
-	$account = $_POST["account"];
-	$PIN = $_POST["PIN"];
 	$action = $_POST["action"];
-	
-	$stmt = $conn->prepare("SELECT id, PIN FROM leaderboard WHERE name = ?");
-	if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
-	$stmt->bind_param("s", $account);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	$stmt->close();
-	$row = $result -> fetch_array(MYSQLI_NUM);
-	
-	if ($result->num_rows == 0){
-		echo 1; 
-		$conn->close();
-		exit();
-	}
-	
-	$lbid = $row[0];
-
-	if ($row[1] != $PIN){
-		echo 2; 
-		$conn->close();
-		exit();
-	}
-	
-	$now = time();
-	$sql = "UPDATE gamestates SET lastupdated = $now";
-	if ($conn->query($sql) === FALSE) { echo "Error updating record: " . $conn->error; }
-	echo 0;
 
 	switch ($action) {
-	case "1":
-		ChangeNick($conn, $lbid, $x, $y);
-		break;
+		case "0":
+			GetAccount($conn);
+			break;
+		case "1":
+			ChangeNick($conn, GetAccount($conn), $x, $y);
+			break;
+		case "2":
+			ChangeAccName($conn, GetAccount($conn));
+			break;
+		case "3":
+			ChangeAccPIN($conn, GetAccount($conn));
+			break;
+		case "4":
+			LogOut($conn, $x, $y);
+			break;
 	}
 
+	$sql = "UPDATE gamestates SET lastupdated = $y";
+	if ($conn->query($sql) === FALSE) { echo "Error updating record: " . $conn->error; }
+
+	$sql = "SELECT id, session_id FROM sessions WHERE session_id=$x";
+	$result = $conn->query($sql);
+	$row = $result -> fetch_array(MYSQLI_NUM);
+	
+	if ($result->num_rows > 0){
+		$universalid = $row[0];
+
+		$sql = "UPDATE sessions SET last_seen=$y WHERE id=$universalid";
+		if ($conn->query($sql) === FALSE) { echo "Error updating record: " . $conn->error; }
+	}
 	$conn->close();
+	
+	function GetAccount($conn){
+		$account = $_POST["account"];
+		$PIN = $_POST["PIN"];
+	
+		$stmt = $conn->prepare("SELECT id, PIN FROM leaderboard WHERE name = ?");
+		if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
+		$stmt->bind_param("s", $account);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$stmt->close();
+		$row = $result -> fetch_array(MYSQLI_NUM);
+	
+		if ($result->num_rows == 0){
+			echo 1; 
+			$conn->close();
+			exit();
+		}
+	
+		$lbid = $row[0];
+
+		if ($row[1] != $PIN){
+			echo 2; 
+			$conn->close();
+			exit();
+		}
+	
+		echo 0;
+		return $lbid;
+	}
 
 	function ChangeNick($conn, $lbid, $x, $y){
-		$name = $_POST["name"];
+		$nickname = $_POST["nickname"];
 
 		$sql = "SELECT id, session_id FROM sessions WHERE session_id=$x";
 		$result = $conn->query($sql);
@@ -62,7 +88,7 @@
 		
 			$stmt = $conn->prepare("INSERT INTO sessions (session_id, lbid, nickname, last_seen) VALUES ($x, $lbid, ?, $y)");
 			if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
-			$stmt->bind_param("s", $name);
+			$stmt->bind_param("s", $nickname);
 			$stmt->execute();
 			$stmt->close();
 	
@@ -75,10 +101,58 @@
 
 			$stmt = $conn->prepare("UPDATE sessions SET nickname = ?, last_seen=$y WHERE id=$universalid");
 			if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
-			$stmt->bind_param("s", $name);
+			$stmt->bind_param("s", $nickname);
 			$stmt->execute();
 			$stmt->close();
-
 		}	
+	}
+
+	function ChangeAccName($conn, $lbid){
+		$accname = $_POST["accname"];
+
+		$stmt = $conn->prepare("SELECT id FROM leaderboard WHERE name = ?");
+		if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
+		$stmt->bind_param("s", $accname);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$stmt->close();
+
+		if ($result->num_rows == 0){
+			$stmt = $conn->prepare("UPDATE leaderboard SET name = ? WHERE id=$lbid");
+			if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
+			$stmt->bind_param("s", $accname);
+			$stmt->execute();
+			$stmt->close();
+		} else { echo 1; }
+	}
+	
+	function ChangeAccPIN($conn, $lbid){
+		$accpin = $_POST["accpin"];
+
+		$stmt = $conn->prepare("UPDATE leaderboard SET PIN = ? WHERE id=$lbid");
+		if ($stmt === FALSE) { echo "Error: " . $stmt . "<br>" . $conn->error; }
+		$stmt->bind_param("s", $accpin);
+		$stmt->execute();
+		$stmt->close();
+	}
+
+	function LogOut($conn, $x, $y){
+		$sql = "SELECT id, session_id FROM sessions WHERE session_id=$x";
+		$result = $conn->query($sql);
+		$row = $result -> fetch_array(MYSQLI_NUM);
+	
+		if ($result->num_rows > 0){
+			$universalid = $row[0];
+
+			$sql = "DELETE FROM sessions WHERE id=$universalid";
+			if ($conn->query($sql) === FALSE) { echo "Error updating record: " . $conn->error; }
+		}
+
+		$sql = "UPDATE gamestates SET lastupdated = $y";
+		if ($conn->query($sql) === FALSE) { echo "Error updating record: " . $conn->error; }
+		
+		$conn->close();
+		echo 0;
+		exit();
 	}
 ?>
